63aa9ad6f35f81a5e8ecc02189d7173ea1331326
Tetiana-UA <izidaxm85@gmail.com>
2023-11-16 09:27:15 +0100

    finish change

--- js/task-05.js
+++ js/task-05.js
@@ -6,8 +6,8 @@ const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", hangleInput);
 
 function hangleInput(event) {
-  spanEl.textContent = inputEl.value;
-  if (inputEl.value === null) {
-    spanEl.textContent = "Anonymous";
-  }
+  spanEl.textContent = inputEl.value || "Anonymous";
+  //if (inputEl.value === null) {
+  //  spanEl.textContent = "Anonymous";
+  //}
 }
